b18369890df148f318e42f22d4097351b628f242
Nasrullah Js <nasrullahjamsarim@gmail.com>
2025-04-27 18:04:58 +0000

    add: example route fix: enable watch mode

--- myApp.js
+++ myApp.js
@@ -1,6 +1,22 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
+
+// app.get("/", function(req, res) {
+//     res.send("Hello Express");
+// })
+
+absolutePath = __dirname + "/views/index.html";
+
+app.get("/", function(req, res) {
+    res.sendFile(absolutePath);
+})
+
+app.use("/public", express.static(__dirname + "/public"));
+
+
+
 
 
 
